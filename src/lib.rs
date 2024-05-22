00eb7b5a358437226611f2e96c29ee9234c7b4b6
martinh <mhofmann@senseinode.com>
2024-05-22 19:44:13 +0800

    updated find_best_player function

--- src/lib.rs
+++ src/lib.rs
@@ -26,9 +26,25 @@ fn draw_card(mut player: &Vec<Player>) {
   todo!("here we assign random value to the field `drawed_card`")
 }
 
-// 4. Determine the best output
-fn find_best_player(all_players: &Vec<Player>) -> Player {
-  todo!()
+// 3. Determine the best output
+fn find_best_player(all_players: &Vec<Player>) -> &Player {
+    let mut best_player: Option<&Player> = None;
+    let mut highest_card_value = 0;
+
+    for player in all_players {
+        if let Some((card, _, _)) = &player.drawed_card {
+            let card_bytes = card.as_output_bytes();
+            let card_sum: u32 = card_bytes.iter().map(|&b| b as u32).sum();
+            let card_value = card_sum % 52;
+
+            if card_value > highest_card_value {
+                highest_card_value = card_value;
+                best_player = Some(player);
+            }
+        }
+    }
+
+    best_player.expect("There should be at least one player with a drawn card")
 }
 
 fn verify_best_player(player: Player) -> bool {
