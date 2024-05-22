e71850958f6e5842052384593804e2b855d21c0e
Kaloyan Gangov <gangov1@gmail.com>
2024-05-22 18:28:20 +0800

    removes unused files from target

--- target/debug/build/proc-macro2-cd0fdc79f20fa6f1/build_script_build-cd0fdc79f20fa6f1.d
+++
@@ -1,5 +0,0 @@
-/Users/gangov/Development/pba/vrf-poker-activity/target/debug/build/proc-macro2-cd0fdc79f20fa6f1/build_script_build-cd0fdc79f20fa6f1: /Users/gangov/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.83/build.rs
-
-/Users/gangov/Development/pba/vrf-poker-activity/target/debug/build/proc-macro2-cd0fdc79f20fa6f1/build_script_build-cd0fdc79f20fa6f1.d: /Users/gangov/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.83/build.rs
-
-/Users/gangov/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.83/build.rs:
