166442c3246104a882ef8413433a1b88c6cff877
Kaloyan Gangov <gangov1@gmail.com>
2024-05-22 17:28:08 +0800

    rustfmt and initial comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
-//todo
+// 1. Generate key pairs for each players
+// 2. Take turn and draw card [x 5] for later
+//   - VRF -> output % 52 = card number
+//   - sign
+// 3. Take turn and commit the output
+// 4. Determine the best output
+// 5. Find the winner from the output
